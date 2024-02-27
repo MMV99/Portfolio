50591274f1de033fd52b3835cdf3203e6f3a495c
Mikko <mikkovaarala@outlook.com>
2024-02-27 22:41:10 +0200

    changed color to blue

--- Portfolio 2.0/script.js
+++ Portfolio 2.0/script.js
@@ -1,9 +1,9 @@
 var q=document.getElementById('matrix'),
     s=window.screen,
     w, h;
-        p=Array(256).join(1).split(''); // Initialize based on initial screen size
-        c=q.getContext('2d'),
-        m=Math;
+        p=Array(256).join(1).split(''); // Initialize based on initial screen size
+        c=q.getContext('2d'),
+        m=Math;
 
 function resizeCanvas() {
     w = q.width = window.innerWidth;
@@ -17,17 +17,17 @@ function resizeCanvas() {
 }
 
 setInterval(function(){
-  c.fillStyle='rgba(0,0,0,0.05)';
-  c.fillRect(0,0,w,h);
-  c.fillStyle='rgba(0,255,0,1)';
-  p=p.map(function(v,i){
-      r=m.random();
-      var str = String.fromCharCode(m.floor(2720+r*33));
-      c.fillText(str,i*10,v);
-      v+=10;
-      var ret = v>768+r*1e4?0:v
-      return ret;
-  });
+  c.fillStyle='rgba(0,0,0,0.05)';
+  c.fillRect(0,0,w,h);
+  c.fillStyle='rgba(0,0,255,1)';
+  p=p.map(function(v,i){
+     r=m.random();
+    var str = String.fromCharCode(m.floor(2720+r*33));
+    c.fillText(str,i*10,v);
+    v+=10;
+    var ret = v>768+r*1e4?0:v
+    return ret;
+  });
 },33);
 
 window.addEventListener('resize', resizeCanvas);
