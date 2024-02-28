3f4594680799982c0bfb22c1e30ec7e13caae097
Mikko <mikkovaarala@outlook.com>
2024-02-28 04:52:04 +0200

    fixed colors

--- Portfolio 2.0/scriptcv.js
+++ Portfolio 2.0/scriptcv.js
@@ -23,7 +23,7 @@ setInterval(function(){
   c.fillRect(0,0,w,h);
 
   //adjust ripple color
-  c.fillStyle='rgba(0,2555,0,1)';
+  c.fillStyle='rgba(0,255,0,1)';
 
   p=p.map(function(v,i){
      r=m.random();
